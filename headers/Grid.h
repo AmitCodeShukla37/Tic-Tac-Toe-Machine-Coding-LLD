a81e486bdf9055d0f6299ceef69e78fd32185f59
AmitShukla2808 <amushuk890@gmail.com>
2026-02-08 18:21:41 +0530

    Header Files For Tic-Tac-Toe Setup

---
+++ headers/Grid.h
@@ -0,0 +1,17 @@
+#ifndef GRID_H
+#define GRID_H
+#include <bits/stdc++.h>
+
+class Grid{
+
+public:
+        int N;
+        std::vector<std::vector<char>>board;
+
+        void CreateGrid();
+
+        void DisplayGrid();
+
+};
+
+#endif
---
+++ headers/Player.h
@@ -0,0 +1,12 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <string>
+
+class Player{
+
+public:
+        char move;
+        std::string name;
+};
+
+#endif
